72aa89fc86b33c379ff5b27f3fe8bed7558ef3dc
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-04-12 18:51:25 +0300

    config-for-db

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
   
 ## Usage
 
-#### FIles backup
-1. first option for files backup is to set list of directories for copy in ```dir-list.txt``` at the root
-2. git
-   
-#### Database backup
-1. 
+1. fill config.json:
+   1. document_root - pwd to project root
+   2. backup_desc - relative path to backup folder (default it is backup folder in root of project)
+   3. backup_src_file - file with list of folders for backup
+2. fill file with dir list to backup (from punctum 1.3)
+3. 
--- backup.py
+++ backup.py
@@ -1,21 +1,47 @@
 import subprocess
 import json
 
-def get_config() -> dict:
-    with open('config.json') as config_file:
-        return json.load(config_file)
+class Config:
+    DOCUMENT_ROOT = 'document_root'
+    BACKUP_SRC = 'backup_src_file'
+    BACKUP_DESC = 'backup_desc'
+    
+    REQUIRED = {DOCUMENT_ROOT, BACKUP_SRC, BACKUP_DESC}
+    
+    def __init__(self) -> None:
+        config_file = open('config.json')
+        self.__config: dict = json.load(config_file)
+        self.__validate()
+    
+    def get(self, key: str, default = None) -> str:
+        return self.__config.get(key, default)
+    
+    def __validate(self) -> None:
+        config_keys = self.__config.keys()
+        for key in self.REQUIRED:
+            if key not in config_keys:
+                raise Exception(f'Fill key `{key}` in config.json')
+
+
+class Rsync:
+    def __init__(self, config: Config) -> None:
+        self.__config = config
+    
+    def execute(self) -> subprocess.CompletedProcess:
+        return subprocess.run(self.__backup_command(), shell=True, stdout=subprocess.PIPE)
+    
+    def __backup_command(self) -> str:
+        return 'rsync ${DEBUG:+-nv} -arR --files-from=%s %s %s' % (
+            self.__config.get(Config.BACKUP_SRC),
+            self.__config.get(Config.DOCUMENT_ROOT),
+            self.__config.get(Config.BACKUP_DESC)
+        )
 
 
 def main():
-    config = get_config()
+    config = Config()
     
-    command = 'rsync ${DEBUG:+-nv} -arR --files-from=%s %s %s' % (
-        config['backup_src_file'],
-        config['document_root'],
-        config['backup_dest']
-    )
-    print(command)
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    result = Rsync(config).execute()
     print(result.stdout)
     print(result.returncode)
 
